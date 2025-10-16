70b7256fe0f63876a81c537fbd621127a7ae728c
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2025-10-16 00:41:44 -0500

    Refactor 2d content layout

--- app/src/main/java/com/charlesmuchene/xr/ui/content/The2DContent.kt
+++ app/src/main/java/com/charlesmuchene/xr/ui/content/The2DContent.kt
@@ -1,16 +1,17 @@
 package com.charlesmuchene.xr.ui.content
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.xr.compose.platform.LocalSession
 import com.charlesmuchene.xr.R
 import com.charlesmuchene.xr.ui.components.FullSpaceModeIconButton
@@ -18,15 +19,18 @@ import com.charlesmuchene.xr.ui.components.FullSpaceModeIconButton
 @Composable
 fun The2DContent(onRequestFullSpaceMode: () -> Unit) {
     Surface {
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            MainContent(modifier = Modifier.padding(48.dp))
+        Box(modifier = Modifier.fillMaxSize()) {
+            MainContent(
+                modifier = Modifier
+                    .padding(48.dp)
+                    .align(Alignment.Center)
+            )
             if (!LocalInspectionMode.current && LocalSession.current != null) {
                 FullSpaceModeIconButton(
                     onClick = onRequestFullSpaceMode,
-                    modifier = Modifier.padding(32.dp)
+                    modifier = Modifier
+                        .padding(32.dp)
+                        .align(Alignment.TopEnd)
                 )
             }
         }
@@ -35,5 +39,9 @@ fun The2DContent(onRequestFullSpaceMode: () -> Unit) {
 
 @Composable
 private fun MainContent(modifier: Modifier = Modifier) {
-    Text(text = stringResource(R.string.normal_is_not_boring), modifier = modifier)
+    Text(
+        text = stringResource(R.string.normal_is_not_boring),
+        modifier = modifier,
+        fontSize = 96.sp
+    )
 }

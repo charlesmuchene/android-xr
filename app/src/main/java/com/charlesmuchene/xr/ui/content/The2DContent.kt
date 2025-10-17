a94af8ef0f0fdebbc47d8464d40f86fb2fed141f
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2025-10-16 22:14:41 -0500

    Add jetliner image to home space

--- app/src/main/java/com/charlesmuchene/xr/ui/content/The2DContent.kt
+++ app/src/main/java/com/charlesmuchene/xr/ui/content/The2DContent.kt
@@ -1,14 +1,28 @@
 package com.charlesmuchene.xr.ui.content
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,6 +35,7 @@ fun The2DContent(onRequestFullSpaceMode: () -> Unit) {
     Surface {
         Box(modifier = Modifier.fillMaxSize()) {
             MainContent(
+                onRequestFullSpaceMode = onRequestFullSpaceMode,
                 modifier = Modifier
                     .padding(48.dp)
                     .align(Alignment.Center)
@@ -38,10 +53,35 @@ fun The2DContent(onRequestFullSpaceMode: () -> Unit) {
 }
 
 @Composable
-private fun MainContent(modifier: Modifier = Modifier) {
-    Text(
-        text = stringResource(R.string.normal_is_not_boring),
-        modifier = modifier,
-        fontSize = 96.sp
-    )
+private fun MainContent(modifier: Modifier = Modifier, onRequestFullSpaceMode: () -> Unit) {
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(24.dp)) {
+        Image(
+            painter = painterResource(R.drawable.jetliner),
+            contentDescription = stringResource(R.string.jetliner),
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(.9f)
+        )
+
+        val infiniteTransition = rememberInfiniteTransition(label = "pulse")
+        val animatedScale by infiniteTransition.animateFloat(
+            initialValue = 1.0f,
+            targetValue = 1.01f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 1000, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            ),
+            label = "pulse-animation"
+        )
+
+
+        Button(
+            onClick = onRequestFullSpaceMode,
+            modifier = Modifier
+                .fillMaxWidth()
+                .scale(animatedScale)
+        ) {
+            Text(stringResource(R.string.main_button), fontSize = 32.sp)
+        }
+    }
 }
